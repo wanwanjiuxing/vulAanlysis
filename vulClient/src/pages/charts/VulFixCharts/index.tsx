9cadfe23c6a7a175d133a379b4003b22164c8a61
Chen Jianyu <chenjianyu@mudu.tv>
2021-12-22 18:11:40 +0800

    2021-12-22

--- vulClient/src/pages/charts/VulFixCharts/index.tsx
+++ vulClient/src/pages/charts/VulFixCharts/index.tsx
@@ -12,7 +12,7 @@ const VulFixCharts: React.FC = () => {
 
 
   const data = [
-    { year: '1991', value: 3 },
+    { year: '1911', value: 3 },
     { year: '1992', value: 4 },
     { year: '1993', value: 3.5 },
     { year: '1994', value: 5 },
